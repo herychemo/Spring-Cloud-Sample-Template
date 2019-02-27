7c3b03361540eec7a23aea8b964ac2af7f89db1c
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-02-27 17:29:14 -0600

    Update ZipkinServer-start.sh

--- DockerServices/ZipkinServer-start.sh
+++ DockerServices/ZipkinServer-start.sh
@@ -1,3 +1,5 @@
-docker run -p 9411:9411 --name ZipkinServer -d openzipkin/zipkin  && docker logs -f ZipkinServer
+docker pull openzipkin/zipkin
 
+docker run -p 9411:9411 --name ZipkinServer -d openzipkin/zipkin  
 
+docker logs -f ZipkinServer
