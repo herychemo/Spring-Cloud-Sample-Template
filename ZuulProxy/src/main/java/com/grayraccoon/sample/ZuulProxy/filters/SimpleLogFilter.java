524a0e07c27a84bd17e13f8724eae0b5c476f353
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-03-11 19:14:31 -0600

    small update {ignorable}

---
+++ ZuulProxy/src/main/java/com/grayraccoon/sample/ZuulProxy/filters/SimpleLogFilter.java
@@ -0,0 +1,40 @@
+package com.grayraccoon.sample.ZuulProxy.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SimpleLogFilter extends ZuulFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleLogFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        LOGGER.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+
+}
