751d508b9b75e3e6644a298269aba9ad765617ee
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-04-26 17:02:22 -0500

    sample scripts for running all, (a lot of services for a single docker machine, let's test using swarm and then K8)

--- DockerServices/start-all-projects.sh
+++ DockerServices/start-all-projects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker rmi $(docker images -qa -f 'dangling=true')
+docker images -qa -f 'dangling=true' || docker rmi $(docker images -qa -f 'dangling=true')
 
 docker network inspect SpringCloudNetwork &> /dev/null || docker network create SpringCloudNetwork
 
@@ -10,7 +10,7 @@ docker run -p 8888:8888     \
     --net SpringCloudNetwork    \
     -d herychemo/sample-cloud-config-server:0.0.1-SNAPSHOT
 
-sleep 4
+sleep 8
 
 docker run -p 8761:8761     \
     --name EurekaServer     \
@@ -18,5 +18,61 @@ docker run -p 8761:8761     \
     -e SPRING_PROFILES_ACTIVE=dockercli \
     -d herychemo/sample-cloud-eureka-server:0.0.1-SNAPSHOT
 
-sleep 1
+sleep 10
 
+docker run -p 9869:9869     \
+    --name auth-ms     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-auth-ms:0.0.1-SNAPSHOT
+
+sleep 6
+
+docker run -p 8081:8081     \
+    --name ZuulApiProxy     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-zuul-api-proxy:0.0.1-SNAPSHOT
+
+sleep 6
+
+docker run -p 9003:9003     \
+    --name TurbineServer     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-turbine-server:0.0.1-SNAPSHOT
+
+sleep 6
+
+docker run -p 9871:9871     \
+    --name accounts-ms     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-accounts-ms:0.0.1-SNAPSHOT
+
+sleep 6
+
+docker run -p 9002:9002     \
+    --name HystrixDashboard     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-hystrix-dashboard:0.0.1-SNAPSHOT
+
+sleep 6
+
+docker run -p 8760:8760     \
+    --name SpringAdminServer     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-spring-admin-server:0.0.1-SNAPSHOT
+
+sleep 4
+
+docker run -p 8080:8080     \
+    --name ui-gateway     \
+    --net SpringCloudNetwork    \
+    -e SPRING_PROFILES_ACTIVE=dockercli \
+    -d herychemo/sample-cloud-ui-gateway:0.0.1-SNAPSHOT
+
+
+sleep 1
--- DockerServices/stop-all-projects.sh
+++ DockerServices/stop-all-projects.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
-docker stop ConfigServer
-docker rm ConfigServer
+docker stop ui-gateway
+docker rm ui-gateway
+
+docker stop SpringAdminServer
+docker rm SpringAdminServer
+
+docker stop HystrixDashboard
+docker rm HystrixDashboard
+
+docker stop accounts-ms
+docker rm accounts-ms
+
+docker stop TurbineServer
+docker rm TurbineServer
+
+docker stop ZuulApiProxy
+docker rm ZuulApiProxy
+
+docker stop auth-ms
+docker rm auth-ms
 
 docker stop EurekaServer
 docker rm EurekaServer
+
+docker stop ConfigServer
+docker rm ConfigServer
 
 
 docker network prune -f
