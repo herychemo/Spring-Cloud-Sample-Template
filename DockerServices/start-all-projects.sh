c839655c42fa753161242acb7b88ffaeedcb378b
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-04-29 09:56:50 -0500

    Changed docker images prefix, from personal repo herychemo/* to custom repo, grayraccoon/*

--- DockerServices/start-all-projects.sh
+++ DockerServices/start-all-projects.sh
@@ -10,7 +10,7 @@ docker network inspect SpringCloudNetwork &> /dev/null || docker network create 
 docker run -p 8888:8888     \
     --name ConfigServer     \
     --net SpringCloudNetwork    \
-    -d herychemo/sample-cloud-config-server:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-config-server:0.0.1-SNAPSHOT
 
 sleep 8
 
@@ -18,7 +18,7 @@ docker run -p 8761:8761     \
     --name EurekaServer     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-eureka-server:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-eureka-server:0.0.1-SNAPSHOT
 
 sleep 10
 
@@ -26,7 +26,7 @@ docker run -p 9869:9869     \
     --name auth-ms     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-auth-ms:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-auth-ms:0.0.1-SNAPSHOT
 
 sleep 6
 
@@ -34,7 +34,7 @@ docker run -p 8081:8081     \
     --name ZuulApiProxy     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-zuul-api-proxy:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-zuul-api-proxy:0.0.1-SNAPSHOT
 
 sleep 6
 
@@ -42,7 +42,7 @@ docker run -p 9003:9003     \
     --name turbine-server     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-turbine-server:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-turbine-server:0.0.1-SNAPSHOT
 
 sleep 6
 
@@ -50,7 +50,7 @@ docker run -p 9871:9871     \
     --name accounts-ms     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-accounts-ms:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-accounts-ms:0.0.1-SNAPSHOT
 
 sleep 6
 
@@ -58,7 +58,7 @@ docker run -p 9002:9002     \
     --name HystrixDashboard     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-hystrix-dashboard:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-hystrix-dashboard:0.0.1-SNAPSHOT
 
 sleep 6
 
@@ -66,7 +66,7 @@ docker run -p 8760:8760     \
     --name SpringAdminServer     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-spring-admin-server:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-spring-admin-server:0.0.1-SNAPSHOT
 
 sleep 4
 
@@ -74,7 +74,7 @@ docker run -p 8080:8080     \
     --name ui-gateway     \
     --net SpringCloudNetwork    \
     -e SPRING_PROFILES_ACTIVE=dockercli \
-    -d herychemo/sample-cloud-ui-gateway:0.0.1-SNAPSHOT
+    -d grayraccoon/sample-cloud-ui-gateway:0.0.1-SNAPSHOT
 
 
 sleep 1
