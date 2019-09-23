7488baa8ee7b353e481a0052dbc4c4c61a38cf27
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-09-23 15:11:15 -0500

    Generic push test

---
+++ push_services.sh
@@ -0,0 +1,17 @@
+
+publish_for() {
+  echo "About to publish ${1}"
+  cd "${1}" || exit
+  mvn $MAVEN_CLI_OPTS dockerfile:push
+  cd .. && echo
+}
+
+publish_for ./AuthMs
+publish_for ./AccountsMs
+publish_for ./ConfigServer
+publish_for ./EurekaServer
+publish_for ./HystrixDashboard
+publish_for ./SpringAdminServer
+publish_for ./turbine-Server
+publish_for ./ui-gateway
+publish_for ./ZuulApiProxy
